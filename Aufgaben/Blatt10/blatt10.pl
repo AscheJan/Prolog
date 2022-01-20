4b928e054ec530b4ebc0d74a1af94cb481de0fd5
Jan Asche <67650687+AscheJan@users.noreply.github.com>
2022-01-20 10:11:15 +0100

    Add files via upload

--- Aufgaben/Blatt10/blatt10.pl
+++ Aufgaben/Blatt10/blatt10.pl
@@ -1,27 +1,37 @@
-
-:- module(pure_literal, [ knf_shell/0, pure_literal/2 ]).
-
 %Jan Asche
 %aschejaq
 %612229
 
-matnr(612229).
+:- module(pure_literal, [ knf_shell/0, pure_literal/2]).
 
-%(c)
 :- use_module(al_def, [al/1]).
-:- use_module(al_nf, [al2menge/2]).
-:- use_module(al_literals, [negate_lit/2]).
+:- use_module(al_nf, [ al2menge/2 ]).
+:- use_module(al_literals, [al_lit/1]).
 
-%(d)
-knf_shell :- read(X), knf_shell_core(X).
+knf_shell:- read(bye).
+knf_shell:- read(X), tescht(X).
 
-knf_shell_core(bye).
-knf_shell_core(X) :- al(X), al2menge(X, Y), write(Y), knf_shell.
 
-%(e)
-pure_literal(KM, KM2) :- is_pure_literal(KM, X), !, findall(Y, pure_literal_cond(KM, X, Y), KM1), pure_literal(KM1, KM2).
-pure_literal(KM, KM).
+tescht(X):- al2menge(al(X), Y), write(Y).
 
-pure_literal_cond(KM, X, Y) :- member(Y, KM), not(member(X, Y)).
 
-is_pure_literal(KM, X) :- append(KM, M), member(X, M), negate_lit(X, NX), not(member(NX, M)).
+pure_literal(X,Y):- is_pure_literal(A,X), reduce(A,X,Y).
+pure_literal(_,[]).
+
+
+is_pure_literal(al_lit(X),[al_lit(X)|_]).
+is_pure_literal(A,[_|X]):- is_pure_literal(A,X).
+
+
+reduce(A,[B|X],[B|Y]):- reduce(A,X,Y), not(enthalten(A,B)), rauskuerzen(A,B,B).
+reduce(A,[B|X],[Y]):- reduce(A,X,Y), enthalten(A,B).
+reduce(A,[B|X],[C|Y]):- reduce(A,X,Y), rauskuerzen(A,B,C).
+
+
+enthalten(A,[A|_]).
+enthalten(A,[_|C]):- enthalten(A,C).
+
+
+rauskuerzen(A,[~ A|B],C):- rauskuerzen(A,B,C).
+rauskuerzen(A,[X|B],[X|C]):- rauskuerzen(A,B,C).
+rauskuerzen(_,[],_).
